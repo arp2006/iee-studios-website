c108aa7ee7de89c4456f2d9da3e70aac0cbd1109
Adityaraj <adityarajpanda.arp@gmail.com>
2026-04-27 00:06:01 +0530

    doubled bg opac

--- app/AsciiBackground.tsx
+++ app/AsciiBackground.tsx
@@ -64,7 +64,7 @@ export default function AsciiBackground() {
       text-[14px]
       leading-[1.2]
       whitespace-pre
-      text-black/[0.035]
+      text-black/[0.07]
       "
     >
       {ascii}
